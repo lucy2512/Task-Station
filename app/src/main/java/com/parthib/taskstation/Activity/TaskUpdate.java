8bbec3b350c5c101b428cf3f0bf3a1790144a0b3
lucy2512 <mail4parthib@gmail.com>
2022-04-07 21:41:57 +0530

    Task Update Layout Created

---
+++ app/src/main/java/com/parthib/taskstation/Activity/TaskUpdate.java
@@ -0,0 +1,16 @@
+package com.parthib.taskstation.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.parthib.taskstation.R;
+
+public class TaskUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_update);
+    }
+}
